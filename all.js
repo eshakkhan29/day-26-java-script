3d52c640b133637070ed793860c71ec6b1bc02fb
eshakkhan29 <eshakkhan29@gmail.com>
2022-01-26 11:32:43 +0600

    first commit

---
+++ all.js
@@ -0,0 +1,83 @@
+// variable decler 
+
+// number, string, boolean
+
+var amar = 20;
+var tar = 40;
+var tarAmar = amar + tar;
+// console.log(tarAmar)
+
+// string er jog 
+
+var firstName = "eshak";
+var lastName = "khan";
+var myName = firstName+" "+lastName
+// console.log(myName);
+
+
+// man poribotton 
+var numbaer1 = 10;
+var namber2 = 18;
+// numbaer1 = numbaer1 + 2;
+// numbaer1 += 2;
+// ek jog korote caile 
+numbaer1 ++;
+var total = numbaer1 + namber2;
+// console.log(total)
+
+// convart string to number purno songkha
+var lebuPrice= "40";
+var aluPrice = "30";
+lebuPrice = parseInt("40")
+aluPrice = parseInt("30")
+var total = lebuPrice + aluPrice;
+// console.log(total)
+
+// vognagso songkha 
+
+var num1 = "10.50";
+var num2 = "5.40";
+num1 = parseFloat("10.50")
+num2 = parseFloat("5.40")
+var total = num1 + num2;
+// console.log(total)
+
+// math operation
+// jog
+var alu = 10;
+var sosa = 20;
+var total = alu + sosa;
+// console.log(total);
+
+// biyog 
+var alu = 50;
+var sosa = 20;
+var total = alu - sosa;
+// console.log(total);
+
+// gun 
+var alu = 10;
+var sosa = 5;
+var total = alu * sosa;
+// console.log(total);
+
+// vag
+var alu = 30;
+var sosa = 5;
+var total = alu / sosa;
+// console.log(total);
+
+// vag ses 
+var alu = 33;
+var sosa = 5;
+var total = alu % sosa;
+// console.log(total);
+
+// vognagso 2 ta jog korte 
+var dim = 7.03;
+var dal = 12.05
+var dimDal = dim + dal;
+// console.log(parseFloat(dimDal.toFixed(1)));
+dimDal = dimDal.toFixed(1);
+dimDal = parseFloat(dimDal);
+console.log(dimDal);
